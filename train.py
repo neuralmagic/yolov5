24fde8c13238d9e90a1efe894c576fd746a70815
Alexandre Marques <alexandre@neuralmagic.com>
2022-10-28 15:27:30 -0400

    Update to account for projection group in optimizer

--- train.py
+++ train.py
@@ -399,7 +399,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
                 xi = [0, nw]  # x interp
                 # compute_loss.gr = np.interp(ni, xi, [0.0, 1.0])  # iou loss ratio (obj_loss = 1.0 or iou)
                 accumulate = max(1, np.interp(ni, xi, [1, nbs / batch_size]).round())
-                for j, x in enumerate(optimizer.param_groups):
+                for j, x in enumerate(optimizer.param_groups[:2]):
                     # bias lr falls from 0.1 to lr0, all other lrs rise from 0.0 to lr0
                     if scheduler:
                         x['lr'] = np.interp(ni, xi, [hyp['warmup_bias_lr'] if j == 2 else 0.0, x['initial_lr'] * lf(epoch)])
