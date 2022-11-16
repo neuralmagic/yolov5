f830854cf7c731e770b146000bea5f0ed803ac48
Konstantin <konstantin@neuralmagic.com>
2022-11-16 14:26:12 +0000

    Update warning text

--- utils/neuralmagic/sparse_train_manager.py
+++ utils/neuralmagic/sparse_train_manager.py
@@ -89,15 +89,15 @@ class SparsificationManager(object):
         if self.checkpoint_manager and self.checkpoint_manager.pruning_modifiers:
             if not self.train_manager:
                 self.log_console(
-                    "Model in danger of de-sparsification. Pruned model was loaded, "
-                    "but no sparsification recipe detected. A recipe with a "
+                    "Pruned model was loaded, but no sparsification recipe detected - "
+                    "model may revert to dense state. A recipe with a "
                     "ConstantPruningModifier can be used to maintain model sparsity "
                     "while training"
                 )
             elif not self.train_manager.pruning_modifiers:
                 self.log_console(
-                    "Model in danger of de-sparsification. Pruned model was loaded, "
-                    "but no pruning modifiers detected in sparsification recipe. A "
+                    "Pruned model was loaded, but no pruning modifiers detected in "
+                    "sparsification recipe - model may revert to dense state. A "
                     "recipe with a ConstantPruningModifier can be used to maintain "
                     "model sparsity while training"
                 )
@@ -382,6 +382,7 @@ class SparsificationManager(object):
             f"Optimizer stripped from {checkpoint_path},"
             f"{f' saved as {save_name},' if save_name else ''} {megabytes:.1f}MB"
         )
+
 
 def maybe_create_sparsification_manager(
     model: torch.nn.Module,
