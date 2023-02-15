e096466939a792c0d5a1659b96d865c7558ce239
Konstantin <konstantin@neuralmagic.com>
2023-02-15 11:06:15 +0000

    initial commit

--- val.py
+++ val.py
@@ -384,7 +384,7 @@ def parse_opt(skip_parse=False):
 
 def main(opt):
     check_requirements(exclude=('tensorboard', 'thop'))
-
+    print_args(vars(opt))
     if opt.task in ('train', 'val', 'test'):  # run normally
         if opt.conf_thres > 0.001:  # https://github.com/ultralytics/yolov5/issues/1466
             LOGGER.info(f'WARNING ⚠️ confidence threshold {opt.conf_thres} > 0.001 produces invalid results')
